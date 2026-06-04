2cd8c4937b5652eaee47625e2e5b67e4b96b6408
Nkosinathi <nkosinathimahlang477@gmail.com>
2026-06-04 10:28:48 +0200

    Create Subtract.java

---
+++ Projects/OOP-Calculator/Subtract.java
@@ -0,0 +1,17 @@
+public class Subtract implements Operator
+{
+
+    @Override
+    public Double getResult(Double... numbers) 
+    {
+        Double sub = numbers[0];       
+        
+        for(int i=1; i < numbers.length; i++)
+        {
+            sub -= numbers[i];
+        }       
+       
+        return sub;
+    }
+    
+}
