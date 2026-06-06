b12145188a2532c6a167dc30f87a1b98b398169c
Nkosinathi <nkosinathimahlang477@gmail.com>
2026-06-06 22:50:29 +0200

    Create Add.java

---
+++ Projects/OOP-Calculator/Add.java
@@ -0,0 +1,13 @@
+public class Add implements Operator
+{
+    @Override
+    public Double getResult(Double... numbers) {
+        Double sum = 0.0;
+        
+        for(Double num: numbers)
+        {
+            sum += num;
+        }        
+        return sum;
+    }    
+}
